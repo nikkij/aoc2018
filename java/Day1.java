d30858dd33a26e5abac44c74a42756f704922088
Jacobson <njacobson@rallydev.com>
2018-12-01 23:27:49 -0700

    Day1 Part 2

--- java/Day1.java
+++ java/Day1.java
@@ -1,20 +1,45 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day1 {
+
+    List<Integer> seen = new ArrayList<>();
+    int found = 0;
+
     public static void main(String args[]) {
         List<Integer> begin = readFile();
+        Day1 day1 = new Day1();
 
-        int answer = reduceFrequencies(begin);
+        int answer = day1.reduceFrequencies(begin, 0);
+
         System.out.println(answer);
     }
 
-    public static int reduceFrequencies(List<Integer> frequencies) {
-        return frequencies.stream().reduce(0, (x,y) -> x+y);
+    public int reduceFrequencies(List<Integer> frequencies, int initValue) {
+
+        int answer = frequencies.stream().reduce(initValue, (x, y) -> {
+            int n = x + y;
+            Optional<Integer> foundIt = seen.stream().filter(e -> e == n).findFirst();
+            if (foundIt.isPresent()) {
+                found = found == 0 ? foundIt.get() : found;
+                return found;
+            } else {
+                seen.add(n);
+                return n;
+            }
+        });
+        
+        if(found != 0) {
+            return found;
+        } else {
+            return reduceFrequencies(frequencies, answer);
+        }
     }
 
     public static List<Integer> readFile() {
@@ -23,8 +48,6 @@ public class Day1 {
 
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
             parsedInput = stream.map(Integer::parseInt).collect(Collectors.toList());
-            //stream.forEach(System.out::println);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
